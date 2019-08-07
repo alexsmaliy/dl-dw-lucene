c4deee34a14cffbd3e1e22eccc7acdde3c9ac396
Alex Smaliy <alex.smaliy@gmail.com>
2019-08-06 22:28:56 -0700

    Fix Jackson serialiation for immutable config object

--- dw-lucene-api/src/main/java/solutions/bloaty/tuts/dw/deepsearch/api/appconfig/DropwizardLuceneDeepSearchConfig.java
+++ dw-lucene-api/src/main/java/solutions/bloaty/tuts/dw/deepsearch/api/appconfig/DropwizardLuceneDeepSearchConfig.java
@@ -1,9 +1,12 @@
 package solutions.bloaty.tuts.dw.deepsearch.api.appconfig;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.dropwizard.Configuration;
 import org.immutables.value.Value;
 
 @Value.Immutable
+@Value.Style(builder = "new")
+@JsonDeserialize(builder = ImmutableDropwizardLuceneDeepSearchConfig.Builder.class)
 public abstract class DropwizardLuceneDeepSearchConfig extends Configuration {
     public abstract String template();
 
