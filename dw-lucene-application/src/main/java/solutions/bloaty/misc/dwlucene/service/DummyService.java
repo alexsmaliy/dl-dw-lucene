b741cfbd4aca26a77535fbdc020201f69cc7ee56
Alex Smaliy <alex.smaliy@gmail.com>
2019-08-07 18:17:20 -0700

    gitignore default app data dir

--- dw-lucene-application/src/main/java/solutions/bloaty/misc/dwlucene/service/DummyService.java
+++ dw-lucene-application/src/main/java/solutions/bloaty/misc/dwlucene/service/DummyService.java
@@ -1,7 +1,31 @@
-Unstaged changes after reset:
-M	build.gradle
-M	dw-lucene-api/build.gradle
-M	dw-lucene-application/src/main/java/solutions/bloaty/misc/dwlucene/DropwizardLuceneDeepSearchApplication.java
-D	dw-lucene-application/src/main/java/solutions/bloaty/misc/dwlucene/resources/DummyResource.java
-M	dw-lucene-application/src/main/java/solutions/bloaty/misc/dwlucene/service/DummyService.java
-M	versions.gradle
+package solutions.bloaty.misc.dwlucene.service;
+
+import io.dropwizard.jersey.params.NonEmptyStringParam;
+import solutions.bloaty.tuts.dw.deepsearch.api.messages.ImmutableSaying;
+import solutions.bloaty.tuts.dw.deepsearch.api.messages.Saying;
+import solutions.bloaty.tuts.dw.deepsearch.api.resource.DummyResource;
+
+import javax.ws.rs.QueryParam;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class DummyService implements DummyResource {
+
+    private final AtomicLong idCounter;
+    private final String template;
+    private final String defaultName;
+
+    public DummyService(String template, String defaultName) {
+        idCounter = new AtomicLong();
+        this.template = template;
+        this.defaultName = defaultName;
+    }
+
+    @Override
+    public Saying sayHello(@QueryParam("name") NonEmptyStringParam maybeName) {
+        String content = String.format(template, maybeName.get().orElse(defaultName));
+        return ImmutableSaying.builder()
+            .id(idCounter.incrementAndGet())
+            .content(content)
+            .build();
+    }
+}
